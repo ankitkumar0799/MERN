63f130a5aae58fbd9b8517155844ed1192763560
ankit <ankitkuswaha07@gmail.com>
2023-08-14 15:22:17 +0530

    how to delete in mern stack

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -6,70 +6,122 @@ var fetchuser = require("../middleware/fetchuser");
 
 //ROute 1 Get All The Notes using GET '/api/auth/fetchallnotes
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
-    try{
-  const notes = await Note.find({ user: req.user.id });
-  res.json(notes);
-    }catch(error){
-      console.log(error.message);
-      res.status(500).send("Some error occured");
-    }
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Some error occured");
+  }
 });
 
 //ROute 1 ass a new Note using POSt '/api/auth/addnote
-router.post("/addnote",fetchuser,[
+router.post(
+  "/addnote",
+  fetchuser,
+  [
     body("title", "Enter a valid title").isLength({ min: 3 }),
-    body("description","Enter a valid description must be atleast 5 characters").isLength({ min: 5 }),],async (req, res) => {
+    body(
+      "description",
+      "Enter a valid description must be atleast 5 characters"
+    ).isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
 
-  try{
-    const {title, description,tag} = req.body
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const savedNote = await note.save();
+
+      res.json(savedNote);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Some error occured");
+    }
+  }
+);
+
+//ROute 3 update a note using put '/api/auth/updatenote login reqiured
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  try {
+    const { title, description, tag } = req.body;
+
+    //craete a newnote object
+    const newNotes = {};
+    if (title) {
+      newNotes.title = title;
+    }
+    if (title) {
+      newNotes.description = description;
+    }
+    if (title) {
+      newNotes.tag = tag;
     }
 
-    const note = new Note({
-title,description,tag, user: req.user.id
-    })
-    const savedNote = await note.save()
+    //find the note to be updated and update it
+    //const note = Note.findByIdAndUp();
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
 
-    res.json(savedNote);
+    //check user is same or not
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
 
-
-}catch(error){
+    // if the upper validation is not working also then the note is exsist
+    note = await Note.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNotes },
+      { new: true }
+    );
+    res.json({ note });
+  } catch (error) {
     console.log(error.message);
     res.status(500).send("Some error occured");
-}
-  
-})
-
-//ROute 3 update a note using POSt '/api/auth/updatenote login reqiured
-router.put("/updatenote/:id",fetchuser,async (req, res) => {
-  const {title,description,tag} = req.body;
-
-  //craete a newnote object
-  const newNotes = {};
-  if(title){newNotes.title = title};
-  if(title){newNotes.description = description};
-  if(title){newNotes.tag = tag};
- 
-
-  //find the note to be updated and update it
-  //const note = Note.findByIdAndUp();
-  let note = await Note.findById(req.params.id);
-  if(!note){return res.status(404).send("Not Found")}
-
-
-
-  //check user is same or not
-  if(note.user.toString() !== req.user.id){
-    return res.status(401).send("Not Allowed");
   }
+});
+
+//ROute 3 delete a note using delete '/api/auth/updatenote login reqiured
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
 
 
-  // if the upper validation is not working also then the note is exsist
-  note = await Note.findByIdAndUpdate(req.params.id,{$set: newNotes},{new:true})
-  res.json({note});
+  try {
+    // //craete a newnote object
+    // const newNotes = {};
+    // if(title){newNotes.title = title};
+    // if(title){newNotes.description = description};
+    // if(title){newNotes.tag = tag};
 
-  })
+    //find the note to be deleted and delete it
+    //const note = Note.findByIdAndUp();
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+
+    //check user is same or not
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+
+    // if the upper validation is not working also then the note is exsist
+    note = await Note.findByIdAndDelete(req.params.id);
+    res.json({ Success: "Note hase been deleted", note: note });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Some error occured");
+  }
+});
 
 module.exports = router;
